f7931f1d9a74612521ad64dc68ca4b3e985d1e83
Zimeng Yang <zimengyang8@gmail.com>
2026-01-15 21:42:57 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by ZM on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
